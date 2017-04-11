88ae9bdd56926bd1f0a7d27d154ec9f2aa2873c2
FinKsu <eeli.haapalainen@live.com>
2017-04-11 20:36:42 +0300

    Turha kommentti

--- First2DGame/src/net/finksu/First2DGame/states/GameState.java
+++ First2DGame/src/net/finksu/First2DGame/states/GameState.java
@@ -21,6 +21,7 @@ public class GameState extends State {
         super(handler);
         
         try{
+            //Tämä on turha kommentti
         this.tiles1 = tiles1;
         this.tiles2 = tiles2;
         world = new World(handler, path, this.tiles1, this.tiles2);
